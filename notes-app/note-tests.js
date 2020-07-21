96dfb2d84b456ee14550b9d821694925d22413e7
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-21 17:51:14 +0100

    2 additional tests created

--- notes-app/note-tests.js
+++ notes-app/note-tests.js
@@ -13,10 +13,28 @@ function expect(a) {
       } else {
         console.log('Fail')
       }
+    },
+    toEqual: function () {
+      if (a === ("")) {
+        console.log('Pass')
+      } else {
+        console.log('Fail')
+      }
+    },
+    toNotEqual: function () {
+      noteModel.set('hello')
+      if (noteModel._text !== ("")) {
+        console.log('Pass')
+      } else {
+        console.log('Fail')
+      }
     }
   }
 }
 
-
 // test
 it(function () {expect(noteModel).beInstanceOf(NoteModel)}, "checks if it's an instance");
+
+it(function () {expect(noteModel._text).toEqual("")}, "checks textbox is empty on creation");
+
+it(function () {expect(noteModel._text).toNotEqual("")}, "checks textbox contains text");
